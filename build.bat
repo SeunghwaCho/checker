3a083e5f85331b6af1d804a034cb7a357939959e
chobocho <chobocho99@gmail.com>
2026-04-16 01:25:36 +0900

    build: add build.bat for automated release packaging

---
+++ build.bat
@@ -0,0 +1,17 @@
+@echo off
+setlocal
+
+cd /d "%~dp0"
+
+set RELEASE_DIR=release
+
+if exist "%RELEASE_DIR%" rmdir /s /q "%RELEASE_DIR%"
+mkdir "%RELEASE_DIR%"
+
+copy style.css "%RELEASE_DIR%\" >nul
+copy app.js    "%RELEASE_DIR%\" >nul
+
+powershell -Command "(Get-Content index.html -Encoding UTF8) -replace '  <script src=""./tests.js""></script>', '' | Set-Content '%RELEASE_DIR%\index.html' -Encoding UTF8"
+
+echo Build done: %RELEASE_DIR%\
+dir /b "%RELEASE_DIR%"
